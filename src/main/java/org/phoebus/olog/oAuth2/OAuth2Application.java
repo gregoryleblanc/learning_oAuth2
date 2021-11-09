5603d495ee69456b457166b2b965ddf805e9e240
Leblanc <leblanc@cas-web-lp001.phy.ohio.edu>
2021-11-09 10:55:53 -0500

    first commit

---
+++ src/main/java/org/phoebus/olog/oAuth2/OAuth2Application.java
@@ -0,0 +1,17 @@
+package org.phoebus.olog.oAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SpringBootApplication
+public class OAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2Application.class, args);
+	}
+
+}
